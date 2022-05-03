439269073853d4ddada4f1ebb78ecbf558720910
Bola Adekoya <debolakoya@yahoo.com>
2022-05-02 22:31:03 -0400

    consolidation and cleanup

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,49 +5,53 @@ using ScriptBuilder.Builders;
 
 var school = new School
 {
-    Id = 1,
-    Name=""
+  Id = 1,
+  Name = ""
 };
 
 var user = new User
 {
-    Amount = 35690,
-    AmountDouble = 356.90,
-    Id = 6,
-    Name = "Bola",
-    Number = 90,
-    ActionDateTime = new DateTime(1990, 1, 1, 12, 10, 30, DateTimeKind.Utc),
-}; 
-//GenerateInsert(user);
+  Amount = 35690,
+  AmountDouble = 356.90,
+  Id = 6,
+  Name = "Bola",
+  Number = 90,
+  ActionDateTime = new DateTime(1990, 1, 1, 12, 10, 30, DateTimeKind.Utc),
+};
+GenerateInsert(user);
+GenerateUpdate(user);
 
 
-var (updateScript, updateParam) = UpdateBuilder.OfType<User>(nameof(User))
-    .AddFields()
-    .Except(nameof(user.Id))
+
+void GenerateInsert(User user1)
+{
+  var (insertScript, insertParam) = Builder.OfType<User>("tbl_Users", "Id")
+      .AddField("Name")
+      .Except(nameof(user1.Id))
+      .Insert(user1);
+
+  Console.WriteLine(insertScript);
+  foreach (var val in insertParam)
+  {
+    Console.WriteLine($"{val.Key}:{val.Value}");
+  }
+}
+
+void GenerateUpdate(User user)
+{
+  var (updateScript, updateParam) = Builder.OfType<User>("mq", "Id")
+    .AddField("Name")
+     .Except(nameof(user.DateofBirth))
     //.Where(nameof(user.Id), Clause.Equals, user.Id)
     //.WhereAnd(nameof(user.Name), Clause.Equals, user.Name)
     //.WhereBetween(nameof(user.Id), 77, 100)
     //.WhereOr(nameof(user.Id), Clause.Equals, user.Name)
-    .Build(user);
+    .Update(user);
 
 
-Console.WriteLine(updateScript);
-foreach (var val in updateParam)
-{
+  Console.WriteLine(updateScript);
+  foreach (var val in updateParam)
+  {
     Console.WriteLine($"{val.Key}:{val.Value}");
-}
-
-void GenerateInsert(User user1)
-{
-    var (insertScript, insertParam) = InsertBuilder.OfType<User>("tbl_Users", "Id")
-        .AddField("Id")
-        .AddFields()
-        .Except(nameof(user1.Id))
-        .Build(user1);
-
-    Console.WriteLine(insertScript);
-    foreach (var val in insertParam)
-    {
-        Console.WriteLine($"{val.Key}:{val.Value}");
-    }
+  }
 }
---
+++ SQLScriptHelper/BuilderExt.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace ScriptBuilder.Builders
+{
+  public static class BuilderExt
+  {
+    public static (IEnumerable<string>, Type) AddField(this (IList<string> fields, Type type) builder, string field)
+    {
+      if (builder.type == null)
+        throw new InvalidOperationException("Builder is not initialized. Please call OfType method first.");
+
+      var property = builder.type.GetProperty(field);
+
+      if (property == null)
+        throw new KeyNotFoundException($"{field} does not exist in {builder.type.Name}");
+
+      builder.fields.Add(property.Name);
+      return builder;
+    }
+
+    public static (IEnumerable<string>, Type) AddFields(this (IEnumerable<string> fields, Type type) builder)
+    {
+      if (builder.type == null)
+        throw new InvalidOperationException("Builder is not initialized. Please call OfType method first.");
+
+      var properties = builder.type.GetProperties();
+      builder.fields = builder.fields.Concat(properties.Select(property => property.Name));
+      return builder;
+    }
+
+    public static (IEnumerable<string>, Type) Except(this (IEnumerable<string> fields, Type type) builder, string field)
+    {
+      if (builder.type == null)
+        throw new InvalidOperationException("Builder is not initialized. Please call OfType method first.");
+
+      var property = builder.type.GetProperty(field);
+
+      if (property == null)
+        throw new KeyNotFoundException($"{field} does not exist in {nameof(builder.type)}");
+
+      var fieldList = builder.fields.ToList();
+      if (fieldList.Contains(field))
+        fieldList.Remove(property.Name);
+      builder.fields = fieldList;
+      return builder;
+    }
+    
+    public static string RemoveLastOccurrence(this string currentString, string valueToRemove, string replaceWith = "")
+    {
+        var index = currentString.LastIndexOf(valueToRemove, StringComparison.OrdinalIgnoreCase);
+        return index == -1
+            ? currentString
+            : currentString.Remove(index, valueToRemove.Length).Insert(index, replaceWith);
+    }
+
+    public static string GetValue(object? value)
+    {
+        return value switch
+        {
+            null => "null",
+            bool => $"{value.ToString()?.ToLower()}",
+            DateTime dateValue => $"'{dateValue:yyyy-MM-dd HH:mm:ss}'",
+            DateOnly dateValue => $"'{dateValue:yyyy-MM-dd}'",
+            TimeOnly dateValue => $"'{dateValue:HH:mm:ss}'",
+            decimal => $"{value}",
+            double => $"{value}",
+            float => $"{value}",
+            int => $"{value}",
+            long => $"{value}",
+            byte[] bytes => $"'{Encoding.UTF8.GetString(bytes)}'",
+            _ => $"'{value}'"
+        };
+    }
+  }
+}
